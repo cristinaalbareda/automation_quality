1d344ed0be25fda4f6c1b754d6c60f6c6d0ab509
Cristina <cristina.albareda@outlook.com>
2023-04-25 17:19:36 -0300

    Login com dados invalidos

---
+++ cypress/e2e/features/login.feature
@@ -0,0 +1,8 @@
+Feature: Login scenarios
+
+    Scenario: Login with wrong information
+        #Login com dados de acesso inválidos
+        Given I acess the homepage
+        When I type a wrong user and password
+        Then it must show a message error
+
--- cypress/support/step_definitions/steps.js
+++ cypress/support/step_definitions/steps.js
@@ -1,4 +1,4 @@
-import {Before, Given, When, And, Then} from "cypress-cucumber-preprocessor/steps"
+import { Before, Given, When, And, Then } from "cypress-cucumber-preprocessor/steps"
 
 Given("I access google page", () => {
     cy.visit("https://www.google.com.br/");
@@ -8,3 +8,17 @@ When("I search for Grupo Quality", () => {
     cy.get('[title="Pesquisar"]').type('Quality Digital{enter}')
 })
 
+Given("I acess the homepage", () => {
+    cy.visit("https://automationexercise.com/")
+    cy.get('.shop-menu > .nav > :nth-child(4) > a').click()
+})
+
+When("I type a wrong user and password", () => {
+    cy.get('[data-qa="login-email"]').type('testemail@gmail.com')
+    cy.get('[data-qa="login-password"]').type('senhaquenaoexiste{enter}')
+})
+
+Then("it must show a message error", () => {
+    cy.wait(500)
+    cy.get('.login-form > form > p').should('have.text', 'Your email or password is incorrect!')
+})
